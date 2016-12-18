390f05b6c5311484e935d8bc997322fb87280f80
slawek <slq@wp.pl>
2016-12-19 00:47:38 +0100

    Webstorm formatting

--- public/app.js
+++ public/app.js
@@ -2,16 +2,16 @@
 
 var learnjs = {};
 
-learnjs.problemView = function() {
-	return $('<div class="problem-view">').text('Coming soon!');
+learnjs.problemView = function () {
+    return $('<div class="problem-view">').text('Coming soon!');
 }
 
-learnjs.showView = function(hash) {
-	var routes = {
-		'#problem-1': learnjs.problemView
-	};
-	var viewFn = routes[hash];
-	if(viewFn) {
-		$('.view-container').empty().append(viewFn());
-	}
+learnjs.showView = function (hash) {
+    var routes = {
+        '#problem-1': learnjs.problemView
+    };
+    var viewFn = routes[hash];
+    if (viewFn) {
+        $('.view-container').empty().append(viewFn());
+    }
 }
