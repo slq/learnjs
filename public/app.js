c8e927c358486ced7cd09a9679cbd1ca9d675cde
slawek <slq@wp.pl>
2016-12-20 17:07:52 +0100

    Routes with parameter

--- public/app.js
+++ public/app.js
@@ -4,14 +4,15 @@ var learnjs = {};
 
 learnjs.problemView = function () {
     return $('<div class="problem-view">').text('Coming soon!');
-}
+};
 
 learnjs.showView = function (hash) {
     var routes = {
-        '#problem-1': learnjs.problemView
+        '#problem': learnjs.problemView
     };
-    var viewFn = routes[hash];
+    var hashParts = hash.split('-');
+    var viewFn = routes[hashParts[0]];
     if (viewFn) {
-        $('.view-container').empty().append(viewFn());
+        $('.view-container').empty().append(viewFn(hashParts[1]));
     }
 }
